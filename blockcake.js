b8559252bbdd32e6a3f4536faa84c49dcfaea68e
Ryan Walmsley <ryanteck@gmail.com>
2013-06-25 20:32:34 +0100

    First Commit

---
+++ blockcake.js
@@ -0,0 +1,31 @@
+/*
+ This file is part of RTK Anti Adblock.
+
+    RTK Anti Adblock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RTK Anti Adblock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RTK Anti Adblock.  If not, see <http://www.gnu.org/licenses/>.
+	
+	===Usage===
+	To use put the advert in a DIV with something unique to detect the advert via, for example I use "advert"
+	Then copy the code used to display the advert, for example I use an inframe to display mine so I copied the iframe code
+	The script will then detect once the page has loaded if the content has been modified then Bam! Your sent to adblockalert.php
+	
+	Made By Ryan Walmsley, @Ryanteck, http://ryanteck.org.uk
+*/
+
+window.onload=function(){
+var advertBlk = document.getElementById("advert");
+var advertcode = 'Your Advert Code Here';
+if(advertBlk.outerHTML != advertcode) {
+console.log("Adblock Detected");
+window.location.replace("adblockalert.html");
+}};
---
+++ readme.md
@@ -0,0 +1,11 @@
+==RTK Anti Adblock==
+This is a simple little script, a HTML document and a template designed to send people who use adblock to a different page (Using low bandwidth and preventing them from accessing the content).
+
+If you like people removing the Adverts that make your site gain money then don't use this code! If you like people removing your adverts from your site making you lose money, Use it!
+
+This is my first main Github project that is designed for public use. If you find a bug just contact me via Twitter (@Ryanteck) or file a bug report.
+
+Also visit my website @ http://blog.ryanteck.org.uk and maybe even donate!
+
+Thanks - 
+Ryan
